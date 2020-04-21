1d3935e585bfc02fdc255f0070f2b177dabf5a30
adityamanjrekar <aditya.manjrekar27@gmail.com>
2020-04-20 21:39:09 -0400

    Put middleware and added code to validate the requrst data

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,25 @@
 const express = require('express');
-
+const { check, validationResult } = require('express-validator');
 const router = express.Router();
 
-// @route  Get api/users
-// @desc   Test route
+// @route  Post api/users
+// @desc   Register user
 // @access public
 
-router.get('/', (req, res) => res.send('User Route'));
+router.post('/', [
+    check('name', 'Name is required')
+        .not()
+        .isEmpty(),
+    check('email', 'Please include email')
+        .isEmail(),
+    check('password', 'Please enter a password greater than six character')
+        .isLength({ min: 6 })
+], (req, res) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+        return res.status(400).json({ errors: error.array() });
+    }
+    res.send('User Route')
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const connectDB = require('./config/db');
 
 connectDB();
 app.get('/', (req, resp) => resp.send('API Running'));
+// Init middle ware
+app.use(express.json({ extended: false }));
 
 //Define routes
 
@@ -13,6 +15,8 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/post', require('./routes/api/post'));
 app.use('/api/profile', require('./routes/api/profile'));
+
+
 
 const PORT = process.env.PORT || 5000;
 
