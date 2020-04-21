333cc8a8b314856301a7cb0946fc35223b05531e
adityamanjrekar <aditya.manjrekar27@gmail.com>
2020-04-20 20:57:11 -0400

    Added code to connect to mongo db

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const app = express();
 
+const connectDB = require('./config/db');
+
+connectDB();
 app.get('/', (req, resp) => resp.send('API Running'));
 
 const PORT = process.env.PORT || 5000;
